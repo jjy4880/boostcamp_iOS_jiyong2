9a8d8ef4d6d2f1c411d3a16a93ffec605a284f45
jjy4880 <jjy48801@naver.com>
2017-07-09 22:15:00 +0900

    6장 수정

--- week2/homework/WorldTrotter_6/WorldTrotter/MapViewController.swift
+++ week2/homework/WorldTrotter_6/WorldTrotter/MapViewController.swift
@@ -10,17 +10,41 @@ import UIKit
 import MapKit
 
 
-class MapViewController: UIViewController {
+
+
+class MapViewController: UIViewController , MKMapViewDelegate , CLLocationManagerDelegate {
     
     //MARK: loadView() 사용
     var mapView: MKMapView!
+    //현재 위치를받기위해.
     
     let locationManager = CLLocationManager()
+    let pinLocation = MKPointAnnotation()
+    let secondPinLocation = MKPointAnnotation()
+    let thirdPinLocation = MKPointAnnotation()
+    
+    
+    
+    
+    
     
     //컨트롤러가 관리하는 뷰를 작성. 뷰를 수동으로 작성.
     override func loadView() {
+        
+        
+        
         //지도 뷰 생성.
         mapView = MKMapView()
+        mapView.delegate = self
+        
+        //현재위치 추가.
+        self.locationManager.delegate = self
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.locationManager.requestWhenInUseAuthorization()
+        self.locationManager.startUpdatingLocation()
+        self.mapView.showsUserLocation = true
+        
+        
         
         //지도 뷰를 이 뷰 컨트롤러의 뷰로 설정
         self.view = mapView
@@ -28,13 +52,11 @@ class MapViewController: UIViewController {
         
         //segmentedcontrol 설정.
         
-        let segmentedControl = UISegmentedControl(items: ["Standard" , "Hybrid" , "Satellite"])
+        let segmentedControl = UISegmentedControl(items: ["Stan" , "Hyb" , "Sat","송도","잠실","가든5","현위치"])
         segmentedControl.backgroundColor = UIColor.white.withAlphaComponent(0.5)
         segmentedControl.selectedSegmentIndex = 0
         
         segmentedControl.addTarget(self, action: #selector(mapTypeChanged), for: .valueChanged)
-        
-       // segmentedControl.addTarget(<#T##target: Any?##Any?#>, action: , for: <#T##UIControlEvents#>)
         //기본 변환 설정 변경 불가.
         segmentedControl.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(segmentedControl)
@@ -45,73 +67,103 @@ class MapViewController: UIViewController {
         let margins = view.layoutMarginsGuide // 8
         let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: margins.leadingAnchor)
         let trailingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
-        
-        
-        
-        
-        
-        
         topConstraint.isActive = true
         leadingConstraint.isActive = true
         trailingConstraint.isActive = true
-        
-    }
     
- 
-    override func viewDidLoad() {
-        super.viewDidLoad()
         
-        
-        //MARK: FirstAnnotation - Songdo CentralPark
-        let location = CLLocationCoordinate2D(latitude: 37.392448, longitude: 126.638831)
-        
-        let annotation = MKPointAnnotation()
-        annotation.coordinate = location
-        annotation.title = "New City Song Do"
-        annotation.subtitle = "CentralPark"
-        
-        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
-        let region = MKCoordinateRegion(center: location, span: span)
-        
-        
-        
-        
-        
-        //MARK: SecondAnnotation - Jamsil - LotteWorld
-        
-        let secondLocation = CLLocationCoordinate2D(latitude: 37.512187, longitude: 127.099558)
-        let secondAnnotation = MKPointAnnotation()
-        secondAnnotation.coordinate = secondLocation
-        secondAnnotation.title = "JamSil"
-        secondAnnotation.subtitle = "LotteWorld"
-        let secondSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
-        let secondRegion = MKCoordinateRegion(center: secondLocation, span: secondSpan)
         
 
-        
-        
-        mapView.setRegion(region, animated: true)
-        mapView.addAnnotation(annotation)
-        mapView.setRegion(secondRegion, animated: true)
-        mapView.addAnnotation(secondAnnotation)
-        
-        locationManager.requestWhenInUseAuthorization()
-        mapView.showsUserLocation = true
-        print("MapViewController loaded its view")
     }
     
+    
     func mapTypeChanged(segControl: UISegmentedControl){
+ 
+        let pinLocation = MKPointAnnotation()
+        let secondPinLocation = MKPointAnnotation()
+        let thirdPinLocation = MKPointAnnotation()
+        
+        //MARK: - FIRST PIN
+        let location = CLLocationCoordinate2D(latitude: 37.392448, longitude: 126.638831)
+        let region = MKCoordinateRegion(center: location, span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
+            pinLocation.coordinate = location
+            pinLocation.title = "newCitySongdo"
+        
+        
+        
+        let secondLocation = CLLocationCoordinate2D(latitude: 37.512187, longitude: 127.099558)
+        let secondRegion = MKCoordinateRegion(center: secondLocation, span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
+            secondPinLocation.coordinate = secondLocation
+            secondPinLocation.title = " JamsilLotteWorld"
+        
+        
+        let thirdLocation = CLLocationCoordinate2D(latitude: 37.478504, longitude: 127.125550)
+        let thirdRegion = MKCoordinateRegion(center: thirdLocation, span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
+            thirdPinLocation.coordinate = thirdLocation
+            thirdPinLocation.title = "Garden5, my Home"
+        
+        
+        
+     
+
         switch segControl.selectedSegmentIndex {
+            
+            
         case 0:
             mapView.mapType = .standard
         case 1:
             mapView.mapType = .hybrid
         case 2:
             mapView.mapType = .satellite
+        case 3:
+            
+            mapView.setRegion(region, animated: true)
+            mapView.addAnnotation(pinLocation)
+            
+        case 4:
+            mapView.setRegion(secondRegion, animated: true)
+            mapView.addAnnotation(secondPinLocation)
+        
+        case 5:
+            mapView.setRegion(thirdRegion, animated: true)
+            mapView.addAnnotation(thirdPinLocation)
+        case 6:
+            locationManager.requestAlwaysAuthorization()
+            locationManager.requestLocation()
+            
         default:
             break
         }
+        
+        
+        
+       
     }
     
     
+    
+    
+    //MARK: - Locations Delegate Method
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error:  \(error.localizedDescription) ")
+    }
+    
+    //MARK: - Locations Delegate Method
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.last
+        let center = CLLocationCoordinate2D(latitude: location!.coordinate.latitude, longitude: location!.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1))
+        
+        self.mapView.setRegion(region, animated: true)
+        self.locationManager.stopUpdatingLocation()
+    }
+ 
+    
+    
+    
+    
 }
+
+
+
+
