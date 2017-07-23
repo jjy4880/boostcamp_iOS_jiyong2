d52d28464bdb221e25c0ac294d941d122c9483aa
jjy4880 <jjy48801@naver.com>
2017-07-24 03:36:40 +0900

    교재 17장 완료

---
+++ week4/TouchTracker_ch17/TouchTracker/Line.swift
@@ -0,0 +1,15 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by User on 2017. 7. 23..
+//  Copyright © 2017년 Ji-Yong Jeong. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
