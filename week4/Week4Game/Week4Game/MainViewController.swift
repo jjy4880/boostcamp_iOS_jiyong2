9a53ceea5c53f6b0ae601b8108e2ebf108cd8ca3
jjy4880 <jjy48801@naver.com>
2017-07-28 13:01:05 +0900

    애니메이션적

--- week4/Week4Game/Week4Game/MainViewController.swift
+++ week4/Week4Game/Week4Game/MainViewController.swift
@@ -13,7 +13,26 @@ class MainViewContrller: UIViewController {
     @IBOutlet weak var mainLabelText: UILabel!
     
     override func viewDidLoad() {
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        // 화면이 나타날 때 애니메이션을 보여주도록 합니다
+        UIView.animate(
+            withDuration: 0.5, delay: 0, options: [.repeat, .curveEaseInOut],
+            animations: {
+                [weak mainLabelText = self.mainLabelText] in
+                mainLabelText?.transform = CGAffineTransform(scaleX: 0.2, y: 0.2)
+            }, completion: nil)
+    }
+ 
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // 화면이 내려갈 때 애니메이션을 중지시킨다 불필요한작동 최소화
+        self.mainLabelText.layer.removeAllAnimations()
+        self.mainLabelText.transform = CGAffineTransform(scaleX: 1, y: 1)
         
     }
+    
     
 }
