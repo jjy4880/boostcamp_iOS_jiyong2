710ad042673b56710710a3864353c5519f2b9056
jjy4880 <jjy48801@naver.com>
2017-07-31 01:25:04 +0900

    여백 수정

--- week5/Photorama_20/Photorama/PhotosViewController.swift
+++ week5/Photorama_20/Photorama/PhotosViewController.swift
@@ -75,7 +75,6 @@ class PhotosViewController: UIViewController, UICollectionViewDelegate, UICollec
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let width = collectionView.frame.width / 4
         return CGSize(width: width, height: width)
-        
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
@@ -83,8 +82,6 @@ class PhotosViewController: UIViewController, UICollectionViewDelegate, UICollec
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return -1
+        return 0
     }
-
-    
 }
