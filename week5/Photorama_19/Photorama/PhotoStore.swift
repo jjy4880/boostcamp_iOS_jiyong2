ae93fe994ff271263bbf678161654c437ad259c2
jjy4880 <jjy48801@naver.com>
2017-07-30 16:24:24 +0900

    19장 과제 , 동메달 완료.

---
+++ week5/Photorama_19/Photorama/PhotoStore.swift
@@ -0,0 +1,80 @@
+//
+//  PhotoStore.swift
+//  Photorama
+//
+//  Created by User on 2017. 7. 29..
+//  Copyright © 2017년 Ji-Yong Jeong. All rights reserved.
+//
+
+import UIKit
+
+enum ImageResult {
+    case success(UIImage)
+    case failure(Error)
+}
+
+enum PhotoError: Error {
+    case imageCreationError
+}
+
+class PhotoStore {
+    let session: URLSession = {
+        let config = URLSessionConfiguration.default
+        return URLSession(configuration: config)
+    }()
+    
+    func fetchRecentPhotos(completion: @escaping (PhotosResult) -> Void) {
+        let url = FlickrAPI.recentPhotoURL()
+        let request = URLRequest(url: url)
+        let task = session.dataTask(with: request) { (data, response, error) -> Void in
+            let result = self.processRecentPhotosRequest(data: data, error: error)
+            
+            // 19장 동메달과제
+            if let httpResponse = response as? HTTPURLResponse {
+                print("statusCode: \(httpResponse.statusCode)")
+                print("headerFields: ")
+                httpResponse.allHeaderFields.forEach { header in
+                    print(header)
+                }
+            }
+            
+            completion(result)
+        }
+        
+        task.resume()
+    }
+    
+    func processRecentPhotosRequest(data: Data?, error: Error?) -> PhotosResult {
+        guard let jsonData = data else {
+            return .failure(error!)
+        }
+        
+        return FlickrAPI.photosFromJSONData(data: jsonData)
+    }
+    
+    func fetchImageForPhoto(photo: Photo, completion: @escaping (ImageResult) -> Void) {
+        let photoURL = photo.remoteURL
+        let request = URLRequest(url: photoURL)
+        let task = session.dataTask(with: request) { (data, response, error) -> Void in
+            let  result = self.processImageRequest(data: data, error: error)
+            if case let .success(image) = result {
+                photo.image = image
+            }
+            
+            completion(result)
+        }
+        
+        task.resume()
+    }
+    
+    func processImageRequest(data: Data?, error: Error?) -> ImageResult {
+        guard let imageData = data, let image = UIImage(data: imageData) else {
+            if data == nil {
+                return .failure(error!)
+            } else {
+                return .failure(PhotoError.imageCreationError)
+            }
+        }
+        return .success(image)
+    }
+}
---
+++ week5/Photorama_19/Photorama/PhotosViewController.swift
@@ -0,0 +1,45 @@
+//
+//  File.swift
+//  Photorama
+//
+//  Created by User on 2017. 7. 29..
+//  Copyright © 2017년 Ji-Yong Jeong. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    var store: PhotoStore!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        store.fetchRecentPhotos() { (photosResult) -> Void in
+            switch photosResult {
+            
+            case let .success(photos):
+                print("Successfully found \(photos.count) recent photos")
+                
+                if let firstPhoto = photos.first {
+                    self.store.fetchImageForPhoto(photo: firstPhoto) { (imageResult) -> Void in
+                        switch imageResult {
+                        case let .success(image):
+                            OperationQueue.main.addOperation {
+                                self.imageView.image = image
+                            }
+                            
+                        case let .failure(error):
+                            print("Error downloading image: \(error)")
+                        }
+                    }
+                }
+                
+            case let .failure(error):
+                print("Error fetching recent photos: \(error)")
+            }
+        } // fetchRecentPotos()
+    } // vieDidLoad
+}
